d011e4da294df9f7ac73f1cda472d66a2f5f5f40
wxxxenly <65085220+wxxxenly@users.noreply.github.com>
2023-04-30 22:55:33 +0800

    Create zadanie 2.go
    Задание 2
    Создать процедуру, сдвигающую элементы k-ого слоя целочисленного прямоугольного
    массива matrixnm на p шагов вправо. Номер слоя k и количество шагов p задать с
    консоли.
    
    Ошибки, которые выводятся: 
    
    # command-line-arguments
    ./main.go:24:6: syntax error: unexpected main, expecting (
    ./main.go:25:5: syntax error: unexpected var at end of statemen

---
+++ zadanie 2.go
@@ -0,0 +1,62 @@
+package main
+import "fmt"
+
+func shiftMatrix(numArray [][]int, n int) {
+
+    rows := len(numArray)
+    cols := len(numArray[0])
+    
+    for i := 0; i < n; i++ {
+        temp := make([]int, n)
+        for j := cols - n; j < cols; j++ {
+            temp[j-cols+n] = numArray[i][j]
+        }
+        for j := cols - 1; j >= n; j-- {
+            numArray[i][j] = numArray[i][j-n]
+        }
+        for j := 0; j < n; j++ {
+            numArray[i][j] = temp[j]
+        }
+        
+    }
+ 
+
+func main() {
+    var rows,cols int
+    rows = 5 //n
+    cols = 4 //m
+    
+    // Заполняем массив значениями, введенными пользователем
+    fmt.Println("Введите элементы матрицы:")
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            fmt.Scan(&numArray[i][j])
+        }
+    }
+    
+    // Выводим массив на экран
+    fmt.Println("Матрица:")
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            fmt.Print(numArray[i][j], "\t")
+        }
+        fmt.Println()
+    }
+    
+    var k,n int
+    fmt.Print("Введите количество шагов: ")
+    fmt.Scanln(&n)
+    //k - номер слоя, n - количество шагов
+    
+    shiftMatrix(numArray[rows][cols],n)
+    
+    // Выводим массив на экран
+    fmt.Println("Матрица:")
+    for i := 0; i < rows; i++ {
+        for j := 0; j < cols; j++ {
+            fmt.Print(numArray[i][j], "\t")
+        }
+        fmt.Println()
+    }
+    
+}
